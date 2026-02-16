1bcb9b050fef56a912c8e07d7319412aa091d2da
Tauhid hossen <tauhidhossen04@gmail.com>
2026-02-16 23:53:38 +0600

    Destructure some items

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,12 +1,19 @@
 "use client"
 
+
+import { useCart } from '@/contexts/CartContext';
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const CheckOutPage = () => {
+
+ const router = useRouter();
+
+ const {items, getTotalPrice, updateQuantity, removeFromCart} = useCart();
+
   return (
     <div>CheckOutPage</div>
   )
 }
 
 export default CheckOutPage
-// nothing today todo
