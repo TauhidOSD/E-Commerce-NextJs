f5ebfbfad8604f245b1a9a1c255d7e75d4763058
Tauhid hossen <tauhidhossen04@gmail.com>
2026-02-17 00:16:30 +0600

    Create loading, error const also UseEffect hoke

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -3,13 +3,24 @@
 
 import { useCart } from '@/contexts/CartContext';
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CheckOutPage = () => {
 
  const router = useRouter();
 
  const {items, getTotalPrice, updateQuantity, removeFromCart} = useCart();
+
+ const [loading, setLoading] = useState(false);
+
+ const [error, setError] = useState(false);
+ const total = getTotalPrice();
+
+ useEffect(() => {
+  if(items.length === 0){
+    router.push("/")
+  }
+ },[items, router])
 
   return (
     <div>CheckOutPage</div>
